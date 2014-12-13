3cbf85f3090a0d43c756df6dcbc7ffdd78c93dda
Uku Taht <uku.taht@gmail.com>
2014-12-13 12:00:21 +0000

    Removed everything web related.

--- project.clj
+++ project.clj
@@ -4,16 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.google.jimfs/jimfs "1.0"]
-                 [enlive "1.1.5"]
-                 [cheshire "5.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.8.13"]
-            [speclj "3.1.0"]]
-  :ring {:handler pinaclj.core.handler/app}
+                 [enlive "1.1.5"]]
+  :plugins [[speclj "3.1.0"]]
   :test-paths ["spec"]
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [clj-time "0.8.0"]
-                        [peridot "0.3.0"]
-                        [speclj "3.1.0"]]}})
+  {:dev {:dependencies [[speclj "3.1.0"]]}})
