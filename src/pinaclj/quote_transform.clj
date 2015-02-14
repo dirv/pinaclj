d713d612e145d8278b32a074d837544564c3d70c
Daniel Irvine <daniel@danielirvine.com>
2015-02-14 14:29:03 +0000

    Refactors quote transform to be slightly more readable

--- src/pinaclj/quote_transform.clj
+++ src/pinaclj/quote_transform.clj
@@ -10,7 +10,7 @@
 (defn- blank? [ch]
   (or (nil? ch) (Character/isWhitespace ch) (= \> ch)))
 
-(defn- replace-char [next-char last-char]
+(defn- replace-quote [next-char last-char]
   (cond
     (and (= \' next-char) (blank? last-char)) "&lsquo;"
     (= \' next-char) "&rsquo;"
@@ -23,21 +23,21 @@
 (defn- in-code-block? [current-tag]
   (= "code" current-tag))
 
-(defn- stream-convert [orig]
-  (:result (reduce
-    (fn [{result :result last-char :last-char in-tag :in-tag in-closing-tag :in-closing-tag current-tag :current-tag} next-char]
-        (cond
-          (and (not in-tag) ( not (in-code-block? current-tag)) (quote-char? next-char)) {:result (str result (replace-char next-char last-char)) :last-char next-char :in-tag in-tag :current-tag current-tag}
-          (= \< next-char) {:result (str result next-char) :last-char next-char :in-tag true :current-tag "" }
-          (= \> next-char)
-            (if in-closing-tag
-                {:result (str result next-char) :last-char next-char :in-tag false :in-closing-tag false :current-tag ""}
-                {:result (str result next-char) :last-char next-char :in-tag false :in-closing-tag false :current-tag current-tag})
-          (and in-tag (= \/ next-char)) { :result (str result next-char) :last-char next-char :in-tag in-tag :in-closing-tag true :current-tag current-tag}
-          in-tag {:result (str result next-char) :last-char next-char :in-tag in-tag :current-tag (str current-tag next-char)}
-          :else {:result (str result next-char) :last-char next-char :in-tag in-tag :current-tag current-tag }))
-    {:result "" :last-char nil :in-tag false :current-tag ""}
-    orig)))
+(defn- set-last-char [cur next-char]
+  (assoc cur :last-char next-char))
+
+(defn- append-char [cur next-char]
+  (assoc cur :result (str (:result cur) next-char)))
+
+(defn- stream-convert [cur next-char]
+  (let [{:keys [in-tag in-closing-tag current-tag result last-char]} cur]
+    (set-last-char (cond
+                     (and (not in-tag) (not (in-code-block? current-tag)) (quote-char? next-char)) (assoc cur :result (str result (replace-quote next-char last-char)))
+                     (= \< next-char) (assoc (append-char cur next-char) :in-tag true :current-tag "")
+                     (= \> next-char) (assoc (append-char cur next-char) :in-tag false :in-closing-tag false :current-tag (if in-closing-tag  "" current-tag))
+                     (and in-tag (= \/ next-char)) (assoc (append-char cur next-char) :in-closing-tag true)
+                     in-tag (assoc (append-char cur next-char) :current-tag (str current-tag next-char))
+                     :else (append-char cur next-char)) next-char)))
 
 (defn convert [text]
-  (stream-convert text))
+  (:result (reduce stream-convert {} text)))
