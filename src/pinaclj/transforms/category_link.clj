4208b506d98348990601a1764777618a55a5277a
Daniel Irvine <daniel@danielirvine.com>
2015-08-19 16:16:47 +0100

    Use retrieve-value for getting category and tag pages

--- src/pinaclj/transforms/category_link.clj
+++ src/pinaclj/transforms/category_link.clj
@@ -1,12 +1,13 @@
 (ns pinaclj.transforms.category-link
   (:require [pinaclj.group :as group]
+            [pinaclj.page :as page]
             [pinaclj.transforms.transforms :as transforms]))
 
 (defn- create-page [category]
   (transforms/apply-all {:title (name category)
                          :destination (group/category-url category)}))
 
-(defn get-category [{category :category} opts]
-  {:page (create-page category)})
+(defn get-category [page opts]
+  {:page (create-page (page/retrieve-value page :category {}))})
 
 (def transform [:category-link get-category])
--- src/pinaclj/transforms/tag_list.clj
+++ src/pinaclj/transforms/tag_list.clj
@@ -1,5 +1,6 @@
 (ns pinaclj.transforms.tag-list
   (:require [pinaclj.group :as group]
+            [pinaclj.page :as page]
             [pinaclj.transforms.transforms :as transforms]))
 
 (defn- to-link-page [tag]
@@ -7,6 +8,6 @@
                          :destination (group/tag-url tag)}))
 
 (defn get-tags [page opts]
-  {:pages (map to-link-page (:tags page))})
+  {:pages (map to-link-page (page/retrieve-value page :tags {}))})
 
 (def transform [:tag-list get-tags])
