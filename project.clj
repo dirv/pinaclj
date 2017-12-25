f07888e9a52c43191459bebc3e44e665b8c606ce
Daniel Irvine <daniel@danielirvine.com>
2017-12-25 12:44:32 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,17 +2,17 @@
   :description "Simple web content system"
   :url "https://github.com/dirv/pinaclj"
   :min-lein-version "2.5.1"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/java.classpath "0.2.2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/java.classpath "0.2.3"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [com.google.jimfs/jimfs "1.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [com.google.jimfs/jimfs "1.1"]
                  [com.taoensso/tower "3.0.2"]
-                 [endophile "0.1.2"]
+                 [endophile "0.2.1"]
                  [enlive "1.1.6"]]
   :eval-in-leiningen true
   :aliases {"test" ["spec"]}
-  :plugins [[speclj "3.3.1"]]
+  :plugins [[speclj "3.3.1"] [lein-deps-tree "0.1.2"]]
   :test-paths ["spec"]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]
                                   [leiningen-core "2.5.3"]]}})
